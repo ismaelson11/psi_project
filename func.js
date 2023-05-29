0fa68c7b8f9f842551068802536aa3f50189d858
ismaelson11 <ismaelson111@gmail.com>
2023-05-29 01:23:28 -0300

    updating FAQ section

--- func.js
+++ func.js
@@ -10,3 +10,20 @@ function mostrarConteudo(conteudoId) {
   var conteudo = document.getElementById(conteudoId)
   conteudo.style.display = "block"
 }
+
+function mostrarTexto(conteudoId) {
+  var conteudos = document.querySelectorAll("pergunta")
+
+  // Oculta todos os conteúdos
+  for (var i = 0; i < conteudos.length; i++) {
+    conteudos[i].style.display = "none"
+  }
+
+  // Exibe o conteúdo com o ID correspondente
+  var conteudo = document.getElementById(conteudoId)
+  if (conteudo.style.display === "block") {
+    conteudo.style.display = "none" // Oculta o conteúdo se estiver visível
+  } else {
+    conteudo.style.display = "block"
+  }
+}
