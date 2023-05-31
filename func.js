1f6e5ffaf514b8c6d8c8fadd3c49eb2bfa104a54
ismaelson11 <ismaelson111@gmail.com>
2023-05-31 01:33:27 -0300

    faq button update

--- func.js
+++ func.js
@@ -1,9 +1,9 @@
 function mostrarConteudo(conteudoId) {
-  var conteudos = document.querySelectorAll('div[id^="conteudo"]')
+  var conteudosMenu = document.querySelectorAll('div[id^="conteudo"]')
 
   // Oculta todos os conteúdos
-  for (var i = 0; i < conteudos.length; i++) {
-    conteudos[i].style.display = "none"
+  for (var i = 0; i < conteudosMenu.length; i++) {
+    conteudosMenu[i].style.display = "none"
   }
 
   // Exibe o conteúdo com o ID correspondente
@@ -12,14 +12,11 @@ function mostrarConteudo(conteudoId) {
 }
 
 function mostrarTexto(conteudoId) {
-  var conteudos = document.querySelectorAll("pergunta")
-
-  // Oculta todos os conteúdos
+  var conteudos = document.getElementsByClassName("pergunta")
   for (var i = 0; i < conteudos.length; i++) {
     conteudos[i].style.display = "none"
   }
 
-  // Exibe o conteúdo com o ID correspondente
   var conteudo = document.getElementById(conteudoId)
   if (conteudo.style.display === "block") {
     conteudo.style.display = "none" // Oculta o conteúdo se estiver visível
